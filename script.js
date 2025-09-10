3ba6786eaad0b99f47dcbc4bdf589ebbe6d858c4
laxmis-prog <laxmisanuu5@gmail.com>
2025-09-10 10:11:48 +0300

    Update padRow function to return the value of concatenating characer variable to the beginning of the name parameter

--- script.js
+++ script.js
@@ -3,14 +3,10 @@ const count = 8;
 const rows = [];
 
 function padRow(name) {
-   return name;
+   return character+ name;
 }
 
-function addTwoNumbers (a,b) {
-   return a+b;
-}
-const sum = addTwoNumbers(5,10);
-console.log(sum);
+
 
 
 const call = padRow("Laxmi");
