5969267477d93dbfca0862b0b17209182925fcd3
laxmis-prog <laxmisanuu5@gmail.com>
2025-09-10 16:05:17 +0300

    Remove name parameter from function declaration, remove Laxmi string from the padRow call and remove both logs

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow(name) {
+function padRow() {
    const test = "Testing";
-   console.log("This works!");
+  
    return test;
-   console.log("This works!");
+   
 }
 
 
-const call = padRow("Laxmi");
+const call = padRow();
 console.log(call);
 
 for (let i = 0; i < count; i = i + 1) {
