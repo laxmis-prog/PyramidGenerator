a127f7a29a3a5443043b835d8b33ce10c3d9d671
laxmis-prog <laxmisanuu5@gmail.com>
2025-09-11 10:47:09 +0300

    Pass i+1 and count as the arguments to padRow call.

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount) {
-   return character.repeat(rowNumber);
-   
+  return character.repeat(rowNumber);
 }
 
-
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(padRow());
+  rows.push(padRow(i+1,count));
 }
 let result = "";
 
